a29871479facd3a20c05dac967dc353d6867c36c
Gaurav Bhambhani <113461154+GauravVBhambhani@users.noreply.github.com>
2025-02-14 22:10:28 -0500

    Initial Commit

---
+++ TinderClone/TinderCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderCloneApp.swift
+//  TinderClone
+//
+//  Created by Gaurav Bhambhani on 2/14/25.
+//
+
+import SwiftUI
+
+@main
+struct TinderCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
